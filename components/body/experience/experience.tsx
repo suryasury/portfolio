210dbf76b32b33878a30e2b23333ff5c8f7209b7
surya <surya11981.sa@gmail.com>
2024-03-09 23:25:32 +0530

    ui channges

--- components/body/experience/experience.tsx
+++ components/body/experience/experience.tsx
@@ -11,17 +11,15 @@ export const Experience = () => {
           <div className={styles.companyCards}>
             <div className={styles.companyHeading}>
               <h3>
-                <span className={styles.role}>
-                  Software Developer - Fullstack
-                </span>{" "}
+                <span className={styles.role}>Software Engineer</span>{" "}
                 <span className={styles.companyName}>
                   @{" "}
                   <a target="_blank" href="https://www.wealth42.com">
-                    Wealth42
+                    CensaNext
                   </a>
                 </span>
               </h3>
-              <p className={styles.range}>July 2022 - Present</p>
+              <p className={styles.range}>November 2022 - Present</p>
             </div>
             <div className={styles.details}>
               <ul>
@@ -33,10 +31,40 @@ export const Experience = () => {
                   Created website that serves as dashboard/utility for
                   operational excellence of core product.
                 </li>
-                <li>
+                {/* <li>
                   Increased customer satisfaction by resolving various bugs and
                   issues on production channel.
+                </li> */}
+              </ul>
+            </div>
+          </div>
+          <div className={styles.companyCards}>
+            <div className={styles.companyHeading}>
+              <h3>
+                <span className={styles.role}>Fullstack Engineer</span>{" "}
+                <span className={styles.companyName}>
+                  @{" "}
+                  <a target="_blank" href="https://www.wealth42.com">
+                    Wealth42
+                  </a>
+                </span>
+              </h3>
+              <p className={styles.range}>July 2022 - November 2022</p>
+            </div>
+            <div className={styles.details}>
+              <ul>
+                <li>
+                  Delivered performance-driven and user-centric websites that
+                  met all business requirements.
                 </li>
+                <li>
+                  Created website that serves as dashboard/utility for
+                  operational excellence of core product.
+                </li>
+                {/* <li>
+                  Increased customer satisfaction by resolving various bugs and
+                  issues on production channel.
+                </li> */}
               </ul>
             </div>
           </div>
@@ -47,7 +75,7 @@ export const Experience = () => {
                 <span className={styles.companyName}>
                   @{" "}
                   <a target="_blank" href="https://www.ghoshak.com">
-                    Zoryboard software solutions
+                    Ghoshak
                   </a>
                 </span>
               </h3>
@@ -60,18 +88,18 @@ export const Experience = () => {
                   realistic coding milestones on pre-release software project
                   development.
                 </li>
-                <li>
+                {/* <li>
                   Led version control efforts for organization, employing public
                   and open source repositories.
-                </li>
+                </li> */}
                 <li>
                   Documented technical workflows and knowledge to educate newly
                   hired employees.
                 </li>
-                <li>
+                {/* <li>
                   Discussed project progress with customers, collected feedback
                   on different stages and directly addressed concerns.
-                </li>
+                </li> */}
               </ul>
             </div>
           </div>
--- components/body/projects/projects.tsx
+++ components/body/projects/projects.tsx
@@ -13,7 +13,14 @@ export const Projects = () => {
         <div className={styles.projectContainer}>
           <div className={styles.projectCard}>
             <div className={styles.projectContent}>
-              <p>Ghoshak POS</p>
+              <div className={styles.projectHeading}>
+                <p>Ghoshak POS </p>{" "}
+                <div className={styles.projectLink}>
+                  <a href="https://billing.ghoshak.store">
+                    <OpenInNewIcon />
+                  </a>
+                </div>
+              </div>
               <h3>Point of sale software</h3>
               <div className={styles.projectDescription}>
                 <p>
@@ -32,11 +39,92 @@ export const Projects = () => {
                 <li>Lambda</li>
                 <li>Cognito</li>
               </ul>
-              <div className={styles.projectLink}>
-                <a href="https://billing.ghoshak.store">
-                  <OpenInNewIcon />
-                </a>
+            </div>
+
+            <div className={styles.projectImageContainer}>
+              <a href="https://billing.ghoshak.store">
+                <div style={{ maxWidth: "700px", display: "block" }}>
+                  <Image
+                    className={styles.projectImage}
+                    src={PosImage}
+                    alt="posImage"
+                    layout="responsive"
+                  />
+                </div>
+              </a>
+            </div>
+          </div>
+          <div className={styles.projectCard} style={{ marginTop: "30px" }}>
+            <div className={styles.projectContent}>
+              <div className={styles.projectHeading}>
+                <p>Ghoshak POS </p>{" "}
+                <div className={styles.projectLink}>
+                  <a href="https://billing.ghoshak.store">
+                    <OpenInNewIcon />
+                  </a>
+                </div>
               </div>
+              <h3>Point of sale software</h3>
+              <div className={styles.projectDescription}>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  commodo tortor nec arcu sollicitudin hendrerit. Nunc pretium
+                  at tellus in sagittis. Sed rutrum posuere magna sodales
+                  condimentum. Nullam id arcu quam. Cras scelerisque sit amet
+                  est non convallis. Aenean in consequat quam. Nunc quis turpis
+                  sollicitudin,
+                </p>
+              </div>
+              <ul className={styles.techList}>
+                <li>React</li>
+                <li>NodeJS</li>
+                <li>DynamoDB</li>
+                <li>Lambda</li>
+                <li>Cognito</li>
+              </ul>
+            </div>
+
+            <div className={styles.projectImageContainer}>
+              <a href="https://billing.ghoshak.store">
+                <div style={{ maxWidth: "700px", display: "block" }}>
+                  <Image
+                    className={styles.projectImage}
+                    src={PosImage}
+                    alt="posImage"
+                    layout="responsive"
+                  />
+                </div>
+              </a>
+            </div>
+          </div>
+          <div className={styles.projectCard} style={{ marginTop: "30px" }}>
+            <div className={styles.projectContent}>
+              <div className={styles.projectHeading}>
+                <p>Ghoshak POS </p>{" "}
+                <div className={styles.projectLink}>
+                  <a href="https://billing.ghoshak.store">
+                    <OpenInNewIcon />
+                  </a>
+                </div>
+              </div>
+              <h3>Point of sale software</h3>
+              <div className={styles.projectDescription}>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  commodo tortor nec arcu sollicitudin hendrerit. Nunc pretium
+                  at tellus in sagittis. Sed rutrum posuere magna sodales
+                  condimentum. Nullam id arcu quam. Cras scelerisque sit amet
+                  est non convallis. Aenean in consequat quam. Nunc quis turpis
+                  sollicitudin,
+                </p>
+              </div>
+              <ul className={styles.techList}>
+                <li>React</li>
+                <li>NodeJS</li>
+                <li>DynamoDB</li>
+                <li>Lambda</li>
+                <li>Cognito</li>
+              </ul>
             </div>
 
             <div className={styles.projectImageContainer}>
